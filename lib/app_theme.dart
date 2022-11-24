ad481b16a6b0be26c0d7cf0bbb9e52c6f720f1ee
carlenee <carleneannabel@gmail.com>
2022-11-24 13:42:53 +0700

    update sdk version and add color theme

---
+++ lib/app_theme.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  AppTheme._();
+
+  // ignore: constant_identifier_names
+  static const Color beige = Color(0xFFEAE7DC);
+  static const Color darkBeige = Color(0xFFD8C3A5);
+  static const Color grey = Color(0xFF8E8D8A);
+  static const Color lightPink = Color(0xFFE98074);
+  static const Color pink = Color(0xFFE85A4F);
+  
+
+}
