f041ec723618a83030832cab0f57ad8f94927e9a
emilicos <notalvaroaustin@gmail.com>
2022-11-24 14:15:02 +0700

    refactor

---
+++ lib/pages/LoginPage.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _loginFormKey = GlobalKey<FormState>();
+  bool isPasswordVisible = false;
+  void togglePasswordView() {
+    setState(() {
+      isPasswordVisible = !isPasswordVisible;
+    });
+  }
+
+  String username = "";
+  String password1 = "";
+  @override
+  Widget build(BuildContext context) {
+    final request = context.watch<CookieRequest>();
+    return Scaffold();
+    // The rest of your widgets are down below
+  }
+}
