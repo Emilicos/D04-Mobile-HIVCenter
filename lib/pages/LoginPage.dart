be24bc7063b0cf7186163291ff8a53addcb0627a
emilicos <notalvaroaustin@gmail.com>
2022-12-04 21:53:53 +0700

    making login forms to prepare

--- lib/pages/LoginPage.dart
+++ lib/pages/LoginPage.dart
@@ -19,11 +19,172 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   String username = "";
-  String password1 = "";
+  String password = "";
   @override
   Widget build(BuildContext context) {
     final request = context.watch<CookieRequest>();
-    return Scaffold();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Login"),
+        ),
+        body: Form(
+            key: _loginFormKey,
+            child: ListView(
+              children: [
+                Padding(
+                  // Menggunakan padding sebesar 8 pixels
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "Username",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          hintText: "Username",
+                          labelText: "Username",
+                          // Menambahkan circular border agar lebih rapi
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0),
+                          ),
+                        ),
+                        // Menambahkan behavior saat nama diketik
+                        onChanged: (String? value) {
+                          setState(() {
+                            username = value!;
+                          });
+                        },
+                        // Menambahkan behavior saat data disimpan
+                        onSaved: (String? value) {
+                          setState(() {
+                            username = value!;
+                          });
+                        },
+                        // Validator sebagai validasi form
+                        validator: (String? value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Username tidak boleh kosong!';
+                          }
+                          return null;
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  // Menggunakan padding sebesar 8 pixels
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "Password",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      TextFormField(
+                        obscureText: !isPasswordVisible,
+                        decoration: InputDecoration(
+                          hintText: "Password",
+                          labelText: "Password",
+                          suffixIcon: GestureDetector(
+                            onTap: () {
+                              togglePasswordView();
+                            },
+                            child: Icon(isPasswordVisible
+                                ? Icons.visibility_off
+                                : Icons.visibility),
+                          ),
+                          // Menambahkan circular border agar lebih rapi
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0),
+                          ),
+                        ),
+                        // Menambahkan behavior saat nama diketik
+                        onChanged: (String? value) {
+                          setState(() {
+                            password = value!;
+                          });
+                        },
+                        // Menambahkan behavior saat data disimpan
+                        onSaved: (String? value) {
+                          setState(() {
+                            password = value!;
+                          });
+                        },
+                        // Validator sebagai validasi form
+                        validator: (String? value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Password tidak boleh kosong!';
+                          }
+                          return null;
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextButton(
+                            style: ButtonStyle(
+                              backgroundColor:
+                                  MaterialStateProperty.all(Colors.blue),
+                            ),
+                            onPressed: () {
+                              if (_loginFormKey.currentState!.validate()) {
+                                showDialog(
+                                  context: context,
+                                  builder: (context) {
+                                    return Dialog(
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(10),
+                                      ),
+                                      elevation: 15,
+                                      child: ListView(
+                                        padding: const EdgeInsets.only(
+                                            top: 20, bottom: 20),
+                                        shrinkWrap: true,
+                                        children: <Widget>[
+                                          const Center(
+                                              child: Text(
+                                                  'Data sudah berhasil dibuat')),
+                                          const SizedBox(height: 20),
+                                          TextButton(
+                                            onPressed: () {
+                                              Navigator.pop(context);
+                                              Navigator.pushReplacementNamed(
+                                                  context, "/homepage");
+                                            },
+                                            child: const Text('Kembali'),
+                                          ),
+                                        ],
+                                      ),
+                                    );
+                                  },
+                                );
+                                // Add sesuatu
+                              }
+                            },
+                            child: const Text(
+                              "Simpan",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            )));
     // The rest of your widgets are down below
   }
 }
