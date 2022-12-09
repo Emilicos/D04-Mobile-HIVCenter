2c226c720443319b36e9fcba8467252d65beaac5
emilicos <notalvaroaustin@gmail.com>
2022-12-09 12:25:34 +0700

    fixing link url

--- lib/utils/delete_blogpost.dart
+++ lib/utils/delete_blogpost.dart
@@ -2,7 +2,7 @@ import 'package:http/http.dart' as http;
 
 Future<http.Response> deleteBlogpost(int id) async {
   // var url = Uri.parse('https://pbp-d04.up.railway.app/blogpost/delete/$id');
-  var url = Uri.parse('https://pbp-d04.up.railway.app/blogpost/delete/$id');
+  var url = Uri.parse('https://pbp-d04.up.railway.app/blogpost/delete/$id/');
   final http.Response response = await http.delete(
     url,
   );
