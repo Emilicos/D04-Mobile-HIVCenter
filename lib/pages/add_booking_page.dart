b913c36e997d110b629cecd0e648f6b2f2a1e68f
Natania Deandra <nataniadeandra@gmail.com>
2022-12-10 22:52:05 +0700

    hore

--- lib/pages/add_booking_page.dart
+++ lib/pages/add_booking_page.dart
@@ -63,18 +63,13 @@ class _AddBookingPageState extends State<AddBookingPage> {
                     if (snapshot.hasData) {
                       var data = snapshot.data!;
                       if (data.isEmpty) {
-                        isVisible = false;
-                        return ListTile(
-                          leading: const Text('Doctors are currently unavailable'),
-                          trailing: TextButton.icon(
-                            icon: const Icon(Icons.exit_to_app),
-                            label: const Text("Back"),
-                            onPressed: () {
-                              Navigator.pushReplacement(
-                                context,
-                                MaterialPageRoute(builder: (context) => const BookingPage())
-                              );
-                            },
+                        return const Center(
+                          child: Text(
+                            'Doctors are currently unavailable',
+                            style: TextStyle(
+                              color: AppTheme.grey,
+                              fontSize: 17.0,
+                            )
                           ),
                         );
                       } else {
@@ -109,7 +104,119 @@ class _AddBookingPageState extends State<AddBookingPage> {
                                   dokter = value!;
                                 });
                               }
-                            )
+                            ),
+
+                            const SizedBox(height: 20.0),
+
+                            ListTile(
+                              title: Text(tanggal.toString()),
+                              leading: TextButton.icon(
+                                icon: const Icon(
+                                  Icons.calendar_today,
+                                  color: AppTheme.grey),
+                                label: const Text(
+                                  "Date",
+                                  style: TextStyle(color: AppTheme.grey)
+                                ),
+                                onPressed: () {
+                                  showDatePicker(
+                                    context: context,
+                                    initialDate: DateTime.now(),
+                                    firstDate: DateTime(2000),
+                                    lastDate: DateTime(2099),
+                                  ).then((date) {
+                                    setState(() {
+                                      tanggal = date.toString().substring(0, 10);
+                                    });
+                                  });
+                                },
+                              ),
+                            ),
+
+                            const SizedBox(height: 20.0),
+
+                            ListTile(
+                              title: Text(waktu),
+                              leading: TextButton.icon(
+                                icon: const Icon(
+                                  Icons.lock_clock,
+                                  color: AppTheme.grey
+                                ),
+                                label: const Text(
+                                  "Time",
+                                  style: TextStyle(color: AppTheme.grey)
+                                ),
+                                onPressed: () {
+                                  showTimePicker(
+                                    context: context,
+                                    initialTime: TimeOfDay.now(),
+                                  ).then((time) {
+                                    setState(() {
+                                      waktu = "${time.toString().substring(10, 15)}:00";
+                                    });
+                                  });
+                                },
+                              ),
+                            ),
+
+                            const SizedBox(height: 20.0),
+
+                            TextButton(
+                              style: ButtonStyle(
+                                backgroundColor:
+                                    MaterialStateProperty.all(AppTheme.lightPink),
+                              ),
+                              onPressed: () async {
+                                if (_formKey.currentState!.validate()) {
+                                  
+                                  await request.post(
+                                      "https://pbp-d04.up.railway.app/booking/add/",
+                                      {
+                                        "doctor": dokter,
+                                        "date": tanggal,
+                                        "time": waktu
+                                      }).then(
+                                      (value) => {
+                                            showDialog(
+                                              context: context,
+                                              builder: (context) {
+                                                return Dialog(
+                                                  shape: RoundedRectangleBorder(
+                                                    borderRadius: BorderRadius.circular(10),
+                                                  ),
+                                                  elevation: 15,
+                                                  child: ListView(
+                                                    padding: const EdgeInsets.only(
+                                                        top: 20, bottom: 20),
+                                                    shrinkWrap: true,
+                                                    children: <Widget>[
+                                                      const Center(
+                                                        child: Text('Apoointment booked successfully')),
+                                                      const SizedBox(height: 20),
+                                                      TextButton(
+                                                        onPressed: () {
+                                                          Navigator.pushReplacement(
+                                                            context,
+                                                            MaterialPageRoute(builder: (context) => const BookingPage())
+                                                          );
+                                                        },
+                                                        child: const Text('Back'),
+                                                      ),
+                                                    ],
+                                                  ),
+                                                );
+                                              },
+                                            )
+                                          },
+                                      );
+                                }
+                              },
+                              child: const Text(
+                                "Save",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+
                           ]
                         ); 
                       }
@@ -119,128 +226,6 @@ class _AddBookingPageState extends State<AddBookingPage> {
                       return const CircularProgressIndicator();
                     }
                   }),
-                ),
-                
-                const SizedBox(height: 12.0),
-                
-                // TANGGAL
-                Visibility(
-                  visible: isVisible,
-                  child: ListTile(
-                    title: Text(tanggal.toString()),
-                    leading: TextButton.icon(
-                      icon: const Icon(
-                        Icons.calendar_today,
-                        color: AppTheme.grey),
-                      label: const Text(
-                        "Date",
-                        style: TextStyle(color: AppTheme.grey)
-                      ),
-                      onPressed: () {
-                        showDatePicker(
-                          context: context,
-                          initialDate: DateTime.now(),
-                          firstDate: DateTime(2000),
-                          lastDate: DateTime(2099),
-                        ).then((date) {
-                          setState(() {
-                            tanggal = date.toString().substring(0, 10);
-                          });
-                        });
-                      },
-                    ),
-                  ),
-                ),
-                
-                const SizedBox(height: 12.0),
-
-                // WAKTU
-                Visibility(
-                  visible: isVisible,
-                  child: ListTile(
-                    title: Text(waktu),
-                    leading: TextButton.icon(
-                      icon: const Icon(
-                        Icons.lock_clock,
-                        color: AppTheme.grey
-                      ),
-                      label: const Text(
-                        "Time",
-                        style: TextStyle(color: AppTheme.grey)
-                      ),
-                      onPressed: () {
-                        showTimePicker(
-                          context: context,
-                          initialTime: TimeOfDay.now(),
-                        ).then((time) {
-                          setState(() {
-                            waktu = "${time.toString().substring(10, 15)}:00";
-                          });
-                        });
-                      },
-                    ),
-                  ),
-                ),
-                
-                const SizedBox(height: 20.0),
-
-                Visibility(
-                  visible: isVisible,
-                  child: TextButton(
-                    style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all(AppTheme.lightPink),
-                    ),
-                    onPressed: () async {
-                      if (_formKey.currentState!.validate()) {
-                        
-                        await request.post(
-                            "https://pbp-d04.up.railway.app/booking/add/",
-                            {
-                              "doctor": dokter,
-                              "date": tanggal,
-                              "time": waktu
-                            }).then(
-                            (value) => {
-                                  showDialog(
-                                    context: context,
-                                    builder: (context) {
-                                      return Dialog(
-                                        shape: RoundedRectangleBorder(
-                                          borderRadius: BorderRadius.circular(10),
-                                        ),
-                                        elevation: 15,
-                                        child: ListView(
-                                          padding: const EdgeInsets.only(
-                                              top: 20, bottom: 20),
-                                          shrinkWrap: true,
-                                          children: <Widget>[
-                                            const Center(
-                                              child: Text('Apoointment booked successfully')),
-                                            const SizedBox(height: 20),
-                                            TextButton(
-                                              onPressed: () {
-                                                Navigator.pushReplacement(
-                                                  context,
-                                                  MaterialPageRoute(builder: (context) => const BookingPage())
-                                                );
-                                              },
-                                              child: const Text('Back'),
-                                            ),
-                                          ],
-                                        ),
-                                      );
-                                    },
-                                  )
-                                },
-                            );
-                      }
-                    },
-                    child: const Text(
-                      "Save",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
                 ),
               ],
             )
