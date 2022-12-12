22a827981a4f3fe5b405cc377c700e0f3b896be3
emilicos <notalvaroaustin@gmail.com>
2022-12-12 13:55:37 +0700

    fixing doctor response

--- lib/utils/fetch_booking_doctor.dart
+++ lib/utils/fetch_booking_doctor.dart
@@ -1,22 +1,16 @@
-import 'dart:convert';
-
-import 'package:http/http.dart' as http;
 import 'package:tk_akhir/models/booking_model.dart';
 
 List<BookingModel> listBookingUser = [];
 
-Future<List<BookingModel>> fetchBookingDokter() async {
+Future<List<BookingModel>> fetchBookingDokter(request) async {
   List<BookingModel> listBookingDokter = [];
 
-  var url = Uri.parse('https://pbp-d04.up.railway.app/booking/appointment/');
-  var response = await http.get(
-    url,
-  );
-  var data = jsonDecode(utf8.decode(response.bodyBytes));
+  var response =
+      await request.get('https://pbp-d04.up.railway.app/booking/appointment/');
 
   List<BookingModel> listBooking = [];
 
-  for (var d in data) {
+  for (var d in response) {
     if (d != null) {
       listBooking.add(BookingModel.fromJson(d));
       listBookingDokter.add(BookingModel.fromJson(d));
